6c92f10c7c1480cbf98c09c402e2d374443811f6
Use your GitHub username <Enter your email address>
2022-01-15 19:08:12 -0700

    update

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
+// variables to be rendered
 const users = [{name:'Helen'}, {name: 'Don'}, {name: 'Linda'}];
 let userExists = false;
 let noUser = false;
@@ -13,18 +14,22 @@ router.get('/', (req, res, next) => {
     path: '/ta02', // For pug, EJS
     activeTA02: true, // For HBS
     contentCSS: true, // For HBS
-    users: users,
-    userExists: userExists,
-    noUser: noUser
+    users: users,//array of users passed to ejs
+    userExists: userExists,//boolean value passed to ejs
+    noUser: noUser//boolean valude passed to ejs
   });
 });
 
 router.post('/addUser', (req, res, next) => {
-  const username = { name: req.body.username };
+  const username = { name: req.body.username }; // stores value received from ejs in variable as key value pair
+  // if checks to see if variable username appears in users array
   if(users.some(user => user.name === req.body.username)) {
+    // if username shows up on list, changes boolean values from above
+    // this allows an alert to show based on boolean value
     noUser = false;
     userExists = true;
   } else {
+    // if username does not show, changes boolean values and pushes new value into the users array
     noUser = false;
     userExists = false;
     users.push(username);
@@ -33,8 +38,10 @@ router.post('/addUser', (req, res, next) => {
 });
 
 router.post('/removeUser', (req, res, next) => {
-  const remUser = req.body.remUsername;
+  const remUser = req.body.remUsername; // stores value received from ejs in variable as key value pair
+  // if checks to see if variable name appears in users array
   if(users.some(user => user.name === req.body.remUsername)) {
+    //if username shows up on list, updates boolean values and removes remUsername from users array
     const index = users.map(function(o) {
       return o.name;
     }).indexOf(remUser);
@@ -44,6 +51,7 @@ router.post('/removeUser', (req, res, next) => {
     userExists = false;
     noUser = false;
   } else {
+    // if username does not show, updates boolean values
     userExists = false;
     noUser = true;
   };
